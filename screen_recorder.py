e8e77b6426d722e0a8c1c6793afdfacfa25cd7c0
cbstorm <bao.ledaocong@gmail.com>
2026-06-08 02:04:01 +0700

    remove some imports

--- screen_recorder.py
+++ screen_recorder.py
@@ -1,10 +1,6 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 import os
 import subprocess
 import time
-import keyboard
 
 
 def main():
